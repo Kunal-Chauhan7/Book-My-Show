e9a0def2d30452c05d0ba9be65676705d0b66722
Kunal Chauhan <kunalchauhanlion@gmail.com>
2023-08-18 22:39:35 +0530

    Inital SetUp Done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 
+//react slick css
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">
+      <h1 className="text-3xl text-indigo-600 tracking-wider">
       Hello world!
     </h1>
     </div>
