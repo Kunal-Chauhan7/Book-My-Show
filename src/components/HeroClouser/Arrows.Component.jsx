e12c23a1e95c6543afb3f4185535b810870bf184
Kunal Chauhan <kunalchauhanlion@gmail.com>
2024-01-07 04:02:45 +0530

    made some progress in heroClouser Component

---
+++ src/components/HeroClouser/Arrows.Component.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export const NextArrow = (props) => {
+  return (
+    <>
+    <div 
+        className={props.className} 
+        style={{...props}} 
+        onClick={props.onClick}/>
+    </> 
+  )
+}
+
+export const PrevArrow = (props) => {
+    <>
+    <div 
+        className={props.className} 
+        style={{...props}} 
+        onClick={props.onClick}/>
+    </>
+  }
